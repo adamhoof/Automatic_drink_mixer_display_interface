41656dfd16fea1b2fb410fbe22f58ff54656c157
adamhoof <adam.hofman1@gmail.com>
2021-05-12 16:01:18 +0200

    fixed false data reading from buffer

--- src/SerialCommunicationHandler.cpp
+++ src/SerialCommunicationHandler.cpp
@@ -14,13 +14,10 @@ void SerialCommunicationHandler::gatherDisplayInputData()
 {
   const uint8_t bufferLength = 2;
   uint8_t inputDataBuffer[bufferLength];
-
-  if (!Ard_Display_Serial.available()){return;}
-
-  else {
-      Ard_Display_Serial.readBytes(inputDataBuffer, bufferLength);
-  }
+  if (Ard_Display_Serial.available()){
+    Ard_Display_Serial.readBytes(inputDataBuffer, bufferLength);
 
     Serial.println(inputDataBuffer[0]);
     Serial.println(inputDataBuffer[1]);
+  }
 }
