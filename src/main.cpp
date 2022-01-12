d41f0d851c7c05072b1332e032cd96b4c3960f6c
adamhoof <adam.hofman1@gmail.com>
2022-01-12 16:29:34 +0100

    add water dispensers

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,7 @@
 #include "DisplayInterfaceHandler.h"
 #include "DrinkPreparator.h"
 #include "SyrupDispensers.h"
+#include "WaterDispensers.h"
 
 CartController cartController {};
 ProximitySensorController proximitySensorController {};
@@ -12,24 +13,37 @@ ScaleController scaleController {};
 DisplayInterfaceHandler displayInterfaceHandler {};
 DrinkPreparator drinkPreparator {};
 SyrupDispensers syrupDispensers{};
+WaterDispensers waterDispensers{};
 
 const uint8_t a = 0;
 const uint8_t b = 1;
 const uint8_t c = 2;
 const uint8_t d = 3;
+const uint8_t right = 1;
+const uint8_t left = 2;
 
 void setup()
 {
+    scaleController.init();
+
+    waterDispensers.setup();
+    waterDispensers.routeState(right, on);
+    waterDispensers.compressorState(on);
+    delay(20000);
+    waterDispensers.compressorState(off);
+    waterDispensers.routeState(right, off);
+
+    /*proximitySensorController.setupControlPins(5, 4);
+    bool isObjectPresent = proximitySensorController.objectIsPresent();
+
     syrupDispensers.setup();
+
     syrupDispensers.openValve(a);
     delay(3000);
     syrupDispensers.closeValve(a);
-    delay(100);
+    delay(100);*/
 
     /*Serial.begin(9600);
-
-    proximitySensorController.setupControlPins(5, 4);
-    bool isObjectPresent = proximitySensorController.objectIsPresent();
 
     scaleController.init();
 
